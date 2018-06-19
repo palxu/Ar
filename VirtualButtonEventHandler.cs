83eb2ebf0846189791b8db39af3b23622df28252
palxu <37163063+palxu@users.noreply.github.com>
2018-06-19 11:22:33 +0800

    Add files via upload

---
+++ VirtualButtonEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class VirtualButtonEventHandler : MonoBehaviour, IVuforiaButtonEventHandler
+{
+    public GameObject vb;
+    public Animator ani;
+    // Use this for initialization
+    void Start()
+    {
+        VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        if (vbb)
+        {
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
